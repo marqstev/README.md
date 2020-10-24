c56b91d30f41403aa9bf5b9d2ff590ab3c8f91d7
Keith Allen <keithjrallen@gmail.com>
2020-10-24 17:12:50 -0400

    accidental

---
+++ app/public/api/members/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM People';
+$vars = [];
+
+if (isset($_GET['memberId'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM People WHERE memberId = ?';
+  $vars = [ $_GET['memberId'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$patients = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($patients, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
