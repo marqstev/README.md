1b9808a8305d0d92e7ae66454002fa5b1b56fff0
Madeline Mopps <mkmopps@iu.edu>
2020-10-25 14:48:48 -0400

    members api
    get api works; post does not work; data connected and appears correctly

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -2,16 +2,9 @@ var app = new Vue({
     el: '#memberPage',
     data:{
       memberList: [],
-      activeMb: null,
-      mbForm: {
-      },
       newMbForm: {}
     },
-    computed: {
-      activeMbName() {
-        return this.activeMb ? this.activeMb.lastName + ', ' + this.activeMb.firstName : ''
-      }
-    },
+
     methods: {
       newMbData() {
         return {
@@ -27,12 +20,13 @@ var app = new Vue({
           primaryPhoneNumber: ""
         }
       },
+
       handleNewMbForm( evt ) {
         // evt.preventDefault();  // Redundant w/ Vue's submit.prevent
 
         // TODO: Validate the data!
 
-        fetch('api/members/post.php', {
+        fetch('api/index/index_post.php', {
           method:'POST',
           body: JSON.stringify(this.newMbForm),
           headers: {
@@ -43,30 +37,18 @@ var app = new Vue({
         .then( json => {
           console.log("Returned from post:", json);
           // TODO: test a result was returned!
-          this.memberList.push(json[0]);
+          this.memberList=json;
+          this.newMbForm=this.newMbData();
         });
-
-        console.log("Creating (POSTing)...!");
-        console.log(this.newMbForm);
-
-        this.newMbForm = this.newMbData();
-      },
-      handleTriageForm( evt ) {
-        console.log("Form submitted!");
-
-        this.mbForm.pt = this.activeMb;
-        console.log(this.mbForm);
-
       }
     },
-    created() {
-      fetch("api/members/")
-      .then( response => response.json() )
-      .then( json => {
-        this.memberList = json;
 
+  created (){
+      fetch("api/index/")
+      .then(response => response.json())
+      .then(json => {
+        this.memberList=json;
         console.log(json)}
-      );
-      this.newMbForm = this.newMbData();
-    }
-  })
+  );
+}
+});
