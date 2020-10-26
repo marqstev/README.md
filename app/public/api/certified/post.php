70bf26397b42bb332e22f198abcc1e8e9c67950c
nnshetty <shetty.nischal19@gmail.com>
2020-10-26 16:31:49 -0400

    Added certified stuff

--- app/public/api/certified/post.php
+++ app/public/api/certified/post.php
@@ -25,4 +25,4 @@ $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.ph
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../certified/?certificationId=' .  $pk');
+header('Location: ../certfied/?certificationId=' .  $pk');
