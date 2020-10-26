aa45df52c172e18a9b6e389a98ed9ba5727daa64
nnshetty <shetty.nischal19@gmail.com>
2020-10-26 16:28:04 -0400

    Latest stuff

---
+++ app/public/api/certified/certifications.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM Certifications';
+$vars = [];
+
+if (isset($_GET['memberId'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM Certifications WHERE certificationId = ?';
+  $vars = [$_GET['certificationId']];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$certified= $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($certified, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
--- app/public/api/login/post.php
+++ app/public/api/login/post.php
@@ -14,8 +14,8 @@ $stmt = $db->prepare(
 $stmt->execute([
   $_POST['loginEmail'],
   $_POST['loginPassword']
- 
-  
+
+
 ]);
 
 // If needed, get auto-generated PK from DB
@@ -25,4 +25,4 @@ $stmt->execute([
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../login/?certificationId=' .  $_POST['certificationId');
+header('Location: ../login/');
